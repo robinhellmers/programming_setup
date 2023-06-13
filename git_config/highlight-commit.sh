db0c6cf79f3cda20ff0a10fdb502ef6e8fc6ea20
Robin Hellmers <robin@hellmers.se>
2023-06-13 08:09:06 +0200

    Additional files

---
+++ git_config/highlight-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPTS_PATH="${HOME}/.local/bin"
+
+source $SCRIPTS_PATH/optionalParameterParser.sh
+_handleArgs $*
+
+BRANCHES=""
+
+FIRST_MATCH_LINE_NUM=$(git lg $OPTIONAL_GIT_LOG_ARGS $BRANCHES | grep -n -m 1 "$FIRST_NON_OPTIONAL_ARG" | cut -d : -f 1)
+git lg $OPTIONAL_GIT_LOG_ARGS $BRANCHES | awk -v commits="$NON_OPTIONAL_ARGS" -f ${HOME}/.local/bin/highlight-commit.awk | less -XR +$FIRST_MATCH_LINE_NUM -j $JUMP_TARGET
---
+++ git_config/optionalParameterParser.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+_handleArgs(){
+    OPTIONAL_GIT_LOG_ARGS=""
+    NON_OPTIONAL_ARGS=""
+    FIRST_NON_OPTIONAL_ARG=""
+    JUMP_TARGET=0
+    while [ "${1:-}" != "" ]; do
+        # NOT optional: neither single dash or double dash
+        if ! [[ "${1:0:1}" == "-" ]] && ! [[ "${1:0:2}" == "--" ]]; then
+            echo "1: ${1} 1:0:1: ${1:0:1}"
+            NON_OPTIONAL_ARGS+=" ${1} "
+        else
+        # Optional: single dash or double dash
+            case "$1" in
+                "-a" | "--all")
+                    OPTIONAL_GIT_LOG_ARGS+=" --all"
+                    ;;
+                "-j" | "--jump-target")
+                    # If number
+                    if [[ $2 =~ ^[0-9]+$ ]]; then
+                        JUMP_TARGET=$(($2))
+                    fi
+                    shift
+                    ;;
+            esac
+        fi
+        shift
+    done
+
+    NON_OPTIONAL_ARGS=$(echo $NON_OPTIONAL_ARGS | sed 's/ *$//')
+    FIRST_NON_OPTIONAL_ARG=$(echo $NON_OPTIONAL_ARGS | awk '{print $1;}')
+    export OPTIONAL_GIT_LOG_ARGS
+    export NON_OPTIONAL_ARGS
+    export FIRST_NON_OPTIONAL_ARG
+    export JUMP_TARGET
+}
+export -f _handleArgs
