619cf29e19967352d4088e1357be76f94fb8c4b8
Robin Hellmers <robin@hellmers.se>
2023-08-07 12:44:52 +0200

    Update to automatic library guard in library template

--- script/setup_scripts/lib/template_lib.bash
+++ script/setup_scripts/lib/template_lib.bash
@@ -1,4 +1,26 @@
-[[ -n $GUARD_BASE ]] && return || readonly GUARD_BASE=1
+#####################
+### Guard library ###
+#####################
+guard_source_max_once()
+{
+    valid_var_name() { grep -q '^[_[:alpha:]][_[:alpha:][:digit:]]*$' <<< "$1"; }
+
+    local file_name="$(basename "${BASH_SOURCE[0]}")"
+    local file_name_wo_extension="${file_name%.*}"
+    local guard_var_name="guard_$file_name_wo_extension"
+
+    if ! valid_var_name "$guard_var_name"
+    then
+        echo "Failed at creating valid variable name for guarding library."
+        echo -e "File name: $file_name\nVariable name: $guard_var_name"
+        exit 1
+    fi
+
+    [[ -n "${!guard_var_name}" ]] && return 1
+    declare -gr "guard_$file_name_wo_extension=true"
+}
+
+guard_source_max_once || return
 
 ##############################
 ### Library initialization ###
