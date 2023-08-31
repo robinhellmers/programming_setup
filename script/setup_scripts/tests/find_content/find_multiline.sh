b60e5b18d571499aac0f757fcd56b9629c31bfea
Robin Hellmers <robin@hellmers.se>
2023-08-31 23:11:26 +0200

    Update test find_multiline()

--- script/setup_scripts/tests/find_content/find_multiline.sh
+++ script/setup_scripts/tests/find_content/find_multiline.sh
@@ -4,89 +4,73 @@ filename="testfile"
 
 main()
 {
-    var="        first line
+    local to_find file_content
+
+    to_find="        first line
     second line  
 third line   "
 
     file_content=$(<$filename)
 
-    find "$var" "$file_content"
+
+    matches=($(find "$file_content" "$to_find"))
+
+    echo "matches:"
+    for ((i=0; i<${#matches[@]}; i++))
+    do
+        echo "$i: ${matches[i]}"
+    done
 }
 
 find()
 {
-    local var="$1"
-    local file_content="$2"
+    local file_content="$1"
+    local to_find="$2"
 
-    local var_no_ws
+    local to_find_wo_ws
     # Remove leading whitespace from only the first line
-    var_no_ws="${var#"${var%%[![:space:]]*}"}"
+    to_find_wo_ws="${to_find#"${to_find%%[![:space:]]*}"}"
     # Remove trailing whitespace from all lines
-    var_no_ws=$(sed -E 's/[[:space:]]*$//' <<< "$var_no_ws")
+    to_find_wo_ws=$(sed -E 's/[[:space:]]*$//' <<< "$to_find_wo_ws")
 
     # Remove trainling whiespace from all lines
     file_content=$(sed -E 's/[[:space:]]*$//' <<< "$file_content")
     
-    local first_line_var
-    first_line_var="$(sed "1q;d" <<< "$var_no_ws")"
+    local first_line_to_find
+    first_line_to_find="$(sed "1q;d" <<< "$to_find_wo_ws")"
 
     # Find potential potential matches
     local first_line_matches
-    first_line_matches_line_num="$(grep -n "$first_line_var" <<< "$file_content" | cut -d : -f 1)"
+    first_line_matches="$(grep -n "$first_line_to_find" <<< "$file_content" | cut -d : -f 1)"
     # Store in array
-    IFS=$'\n' first_line_matches_line_num=($first_line_matches_line_num)
+    IFS=$'\n' first_line_matches=($first_line_matches)
 
-    echo "first_line_matches_line_num[@]:"
-    echo "${first_line_matches_line_num[@]}"
-
-    local matching_starting_line_numbers=()
-    for line_num in "${first_line_matches_line_num[@]}"
-    do  
-        echo "Looking at line number: $line_num"
+    local matches_line_numbers_start=()
+    for line_num in "${first_line_matches[@]}"
+    do
+        # For each first line match, start checking rest of lines to match
         local rel_line_num=1
         local content_is_matching='true'
-        while IFS= read -r look_for_line
+        while IFS= read -r look_for_line # <<< "$to_find_wo_ws"
         do
-            if (( rel_line_num == 1))
-            then
-                ((rel_line_num++))
-                continue
-            elif [[ "$content_is_matching" != 'true' ]]
-            then
-                continue
-            fi
+            (( rel_line_num == 1)) && { ((rel_line_num++)); continue; }
+
+            [[ "$content_is_matching" != 'true' ]] && break
             
             local file_line
             file_line="$(sed "$((line_num + rel_line_num - 1))q;d" <<< "$file_content")"
 
-            echo "file_line:"
-            echo "'$file_line'"
-            echo "look_for_line:"
-            echo "'$look_for_line'"
-            echo
-
-            if [[ "$file_line" == "$look_for_line" ]]
-            then
-                echo "Lines are equal!"
-            else
-                echo "Lines are NOT equal!"
-                content_is_matching='false'
-            fi
-
-            echo ""
+            [[ "$file_line" != "$look_for_line" ]] && content_is_matching='false'
             
             ((rel_line_num++))
-        done <<< "$var_no_ws"
+        done <<< "$to_find_wo_ws"
 
         if [[ "$content_is_matching" == 'true' ]]
         then
             matching_starting_line_numbers+=("$line_num")
-            echo -e "Content found starting at line: $line_num\n"
         fi
     done
 
-    echo -e "\n\n"
-    echo "All matching content start line numbers:"
     echo "${matching_starting_line_numbers[@]}"
 }
 
